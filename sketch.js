ab678c96eebc085c73676a5dc56b9453e5b20404
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 09:58:45 -0400

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(500, 500);
+  background(220, 220, 220);
+  strokeWeight(10);
+  stroke(255,0,255);
+  point(20,20);
+  line(100,100,400,400);
+  fill(0,255,0);
+  rect(200,200,120,190);
+  fill(255,0,0);
+  ellipse(180,200,200,100);
+  
+}
+// What do the following functions take as parameters?
+// createCanvas(width, height)
+// background(red, green, blue)
+// point(x, y)
+// line(startX, startY, endX, endY)
+// ellipse( x, y, width, height)
+// rect( topLeftX, topLeftY, width, height)
+// fill(red, green, blue)
+// stroke(red, green, blue)
